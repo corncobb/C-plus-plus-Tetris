23fc5cb35de833b359bf57caf32a6fa96b02c6e3
Cameron Cobb <cameronjcobb@gmail.com>
2019-08-10 21:39:10 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+std::wstring tetromino[7];
+
+int nFieldWidth = 12;
+int nFieldHeight = 18;
+unsigned char *pField = nullptr;
+
+int Rotate(int px, int py, int r);
+
+int main() 
+{
+    //create assets
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+
+    tetromino[1].append(L"..X.");
+    tetromino[1].append(L".XX.");
+    tetromino[1].append(L".X..");
+    tetromino[1].append(L"....");
+
+    tetromino[2].append(L".X..");
+    tetromino[2].append(L".XX.");
+    tetromino[2].append(L"..X.");
+    tetromino[2].append(L"....");
+
+    tetromino[3].append(L"....");
+    tetromino[3].append(L".XX.");
+    tetromino[3].append(L".XX.");
+    tetromino[3].append(L"....");
+
+    tetromino[4].append(L"..X.");
+    tetromino[4].append(L".XX.");
+    tetromino[4].append(L"..X.");
+    tetromino[4].append(L"....");
+
+    tetromino[5].append(L"....");
+    tetromino[5].append(L".XX.");
+    tetromino[5].append(L"..X.");
+    tetromino[5].append(L"..X.");
+
+    tetromino[6].append(L"....");
+    tetromino[6].append(L".XX.");
+    tetromino[6].append(L".X..");
+    tetromino[6].append(L".X..");
+ 
+}
+
+int Rotate(int px, int py, int r)
+{
+    switch (r % 4)
+    {
+        case 0: return py * 4 + px;             // 0 degrees
+        case 1: return 12 + py - (px * 4);      // 90 degrees
+        case 2: return 15 - (py * 4) - px;      // 180 degrees
+        case 3: return 3 - py + (px * 4);       // 270 degrees
+    }
+    return 0;
+}
